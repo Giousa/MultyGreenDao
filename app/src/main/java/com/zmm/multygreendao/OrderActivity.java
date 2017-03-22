7467e0f5ddf418f1d5928974ca1cc0debe613bda
Giousa <65489469@qq.com>
2017-03-22 15:41:33 +0800

    关联表中添加和查询的简单应用

--- app/src/main/java/com/zmm/multygreendao/OrderActivity.java
+++ app/src/main/java/com/zmm/multygreendao/OrderActivity.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
+import com.zmm.multygreendao.gen.CustomerDao;
 import com.zmm.multygreendao.gen.OrderDao;
 import com.zmm.multygreendao.manager.GreenDaoManager;
+import com.zmm.multygreendao.model.Customer;
+import com.zmm.multygreendao.model.Order;
 import com.zmm.multygreendao.utils.LogUtils;
+import com.zmm.multygreendao.utils.ToastUtils;
+
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -38,6 +45,8 @@ public class OrderActivity extends AppCompatActivity {
     private InputMethodManager mInputMethodManager;
     private long mCustomerID;
     private OrderDao mOrderDao;
+    private CustomerDao mCustomerDao;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,6 +57,7 @@ public class OrderActivity extends AppCompatActivity {
         mCustomerID = getIntent().getLongExtra(KEY_ID, 0);
         LogUtils.d("order keyId = "+ mCustomerID);
         mOrderDao = GreenDaoManager.getInstance().getOrderDao();
+        mCustomerDao = GreenDaoManager.getInstance().getCustomerDao();
         initView();
     }
 
@@ -61,14 +71,43 @@ public class OrderActivity extends AppCompatActivity {
 
         switch (view.getId()) {
             case R.id.add:
+                insertData();
                 break;
             case R.id.update:
                 break;
             case R.id.query:
+                queryData();
                 break;
             case R.id.delete:
                 break;
         }
+    }
+
+    private void queryData() {
+        List<Order> orderList = mOrderDao.loadAll();
+        if(orderList != null && orderList.size()>0){
+            for (Order order:orderList) {
+                long customerId = order.getCustomerId();
+                Customer customer = mCustomerDao.load(customerId);
+                LogUtils.d("name = "+customer.getName()+",age = "+customer.getAge()+",url = "+order.getUrl());
+            }
+        }
+    }
+
+    private void insertData() {
+        if (TextUtils.isEmpty(mUrl.getText())) {
+            ToastUtils.SimpleToast("姓名不能为空!");
+            return;
+        }
+
+        if (TextUtils.isEmpty(mType.getText())) {
+            ToastUtils.SimpleToast("年龄不能为空!");
+            return;
+        }
+
+        Order order = new Order(null,mCustomerID,"~~~",mUrl.getText().toString(),Integer.parseInt(mType.getText().toString()));
+        long insert = mOrderDao.insert(order);
+        LogUtils.d("Order 添加数据成功:" + insert);
     }
 
     /**
